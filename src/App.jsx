375cbc4af4a9a9b4ad3d29c1ea90ed1fa9542e7d
nadafathma <tonadafathma@gmail.com>
2024-09-09 23:05:23 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Counter from './Counter';
+
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-light'>Counter App</h1>
+    <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Counter.jsx
@@ -0,0 +1,37 @@
+import React,{useState} from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import { increment,decrement,reset,countwithNum } from './Redux/CounterSlice';
+
+
+function Counter() {
+
+    const count=useSelector((store)=>store.counterReducer.counter)
+    console.log(count);
+
+    const dispatch=useDispatch()
+
+    const [num,setNum]=useState(0)
+
+  return (
+    <>
+      <div className='d-flex justify-content-center align-items-center' style={{height:"80vh"}}>
+        <div className='w-75 border border-2 border-light p-3'>
+            <h3 className='text-center text-danger'>Counter</h3>
+            <h2 className='text-center text-light'>{count}</h2>
+            <div className='d-flex justify-content-center'>
+                <button className='btn btn-success me-3' onClick={()=>{dispatch(increment())}}>+ Increment</button>
+                <button className='btn btn-success me-3' onClick={()=>{dispatch(decrement())}}>- Decrement</button>
+                <button className='btn btn-success me-3' onClick={()=>{dispatch(reset())}}>Reset</button>
+            </div>
+            <div className='my-3 d-flex justify-content-around'>
+                <input type="number" onChange={(e)=>{setNum(e.target.value)}} placeholder='Enter Number' className='form-control'  />
+                <button className='btn btn-primary' onClick={()=>{dispatch(countwithNum(num))}}>Count</button>
+            </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Counter
